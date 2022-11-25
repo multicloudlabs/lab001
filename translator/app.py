ebe2edc32151a337796293bb58f7769f5a2986f9
multicloudlabs <60463426+multicloudlabs@users.noreply.github.com>
2022-11-25 13:09:30 +0100

    Update of otel collector config and app services

--- translator/app.py
+++ translator/app.py
@@ -7,10 +7,9 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.semconv.trace import SpanAttributes
 import configparser
-import json
 import requests
 
 
@@ -27,9 +26,13 @@ model_id = configparser['DEFAULT']['model_id']
 # Create app
 app = Flask(__name__)
 
+# Set otel service name
+resource = Resource(attributes={
+    SERVICE_NAME: "app_service"
+})
 
 # Initialize tracing and an exporter
-provider = TracerProvider()
+provider = TracerProvider(resource=resource) 
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
@@ -41,38 +44,60 @@ RequestsInstrumentor().instrument()
 
 chat_history = []
 
+@app.route("/healthcheck", methods=['GET'])
+@tracer.start_as_current_span("/healthcheck")
+def healthcheck():
+    return __name__
+
 @app.route("/")
 def index():
-    return render_template("./index.html")
+    with tracer.start_as_current_span("index()") as span:
+        return render_template("./index.html")
+
 
 @app.route("/translate", methods=['GET',"POST"])
 def process_request():
-    if request.method == "GET":
-        #chat_history.append(msg)
+    with tracer.start_as_current_span("process_request()") as span:
+        if request.method == "GET":
+            with tracer.start_as_current_span("process_request(): GET /translate") as span:
+                current_span=trace.get_current_span()
+                current_span.set_attribute(SpanAttributes.HTTP_METHOD, "GET")
+                current_span.set_attribute(SpanAttributes.HTTP_URL, "http://"+host+":"+port+"/translate")  
+                #chat_history.append(msg)
 
-        chat_history.append(" ")
+                chat_history.append(" ")
 
-        history_copy = chat_history.copy()
-        history_copy.reverse()
+                history_copy = chat_history.copy()
+                history_copy.reverse()
 
-        return render_template("./translate.html", messages=history_copy)
-    else:
-        # Get input sentence
-        input_sentence = request.form.get("input")
+                return render_template("./translate.html", messages=history_copy)
+        else:
+            with tracer.start_as_current_span("process_request(): POST /translate") as span:
+                current_span=trace.get_current_span()
+                current_span.set_attribute(SpanAttributes.HTTP_METHOD, "POST")
+                current_span.set_attribute(SpanAttributes.HTTP_URL, "http://"+host+":"+port+"/translate")  
+                # Get input sentence
+                with tracer.start_as_current_span("process_request(): get form input") as span:
+                    input_sentence = request.form.get("input")
+                    current_span.set_attribute("input_sentence=", input_sentence)
+                    current_span.set_attribute("model_id=", model_id)
 
-        url = "http://127.0.0.1:" + server_port+ "/api/translate"
-        response = requests.post(url=url,params={'input_sentence':input_sentence,'language_model':model_id})
+                with tracer.start_as_current_span("process_request(): call translate server") as span:
+                    url = "http://127.0.0.1:" + server_port+ "/api/translate"
+                    response = requests.post(url=url,params={'input_sentence':input_sentence,'language_model':model_id})
 
-        output_sentence = response.text
+                    output_sentence = response.text
+                    current_span.set_attribute("output_sentence=", output_sentence)
 
-        chat_history.append("PL: " +str(output_sentence))
-        chat_history.append("EN: " +input_sentence)
-        chat_history.append("***")
-    
-        history_copy = chat_history.copy()
-        history_copy.reverse()
+                with tracer.start_as_current_span("process_request(): prepare output") as span:
+                    chat_history.append("PL: " +str(output_sentence))
+                    chat_history.append("EN: " +input_sentence)
+                    chat_history.append("***")
+                
+                    history_copy = chat_history.copy()
+                    history_copy.reverse()
 
-        return render_template("./translate.html", messages = history_copy)
+                return render_template("./translate.html", messages = history_copy)
 
 if __name__ == "__main__":
     app.run(host=host, port=port, debug=True, threaded=False)
